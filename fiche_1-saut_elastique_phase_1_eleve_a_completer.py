02231c06546e0b7eb791ff39f6fd20be5e595db2
Laurent Abbal <laurent@abbal.com>
2019-08-15 10:28:17 +0900

    Add files via upload

---
+++ fiche_1-saut_elastique_phase_1_eleve_a_completer.py
@@ -0,0 +1,35 @@
+﻿# -*- coding: utf-8 -*-
+
+# Importation de la librairie nécessaire pour les tracés
+ 
+
+# Définition des constantes g et m
+
+# Constante physique : g = 9.81 m/s²
+
+
+# Paramètre : masse du sauteur en kg
+
+
+# Création des listes vides
+    # liste pour stocker les valeurs des hauteurs de chute
+    # liste pour stocker les valeurs des énergies potentielles de pesanteur
+
+# Ajout itératif des valeurs de hauteurs de chute dans la liste h
+        # les valeurs s'étendent de 0 m à 31 m exclu par pas de 1 m 
+        # ajout d'un élèment à la fin de la liste h
+
+# Calcul et ajout itératif des valeurs de l'énergie potentielle de pesanteur Epp(J) correspondantes aux hauteurs de chute
+        # les valeurs s'étendent de 0 m à 31 m exclu par pas de 1 m 
+        # ajout d'un élèment à la fin de la liste h
+    
+ 
+# Tracé de l'énergie potentielle de pesanteur et de l'énergie cinétique en fonction de la hauteur
+        # plot(abscisse,ordonnée,"legende de la courbe")
+        # xlabel("Nom de l'axe de l'axe des absicces")
+        # ylabel("Nom de l'axe des ordonnées")
+        # Position de la legende de la courbe dans le graphique
+        # Affiche la grille du graphique
+        # Affiche le graphique
+
+
